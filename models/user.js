98e9ab0dcce5fe16e18016e2534707914e0bb573
ecopuls <ecopuls2023@gmail.com>
2025-08-11 20:09:28 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,9 +1,28 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    username: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-});
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: String,
+  isGoogleUser: {
+    type: Boolean,
+    default: false,
+  },
+  profilePic: {
+    type: String,
+    default: ""
+  },
+  isAdmin: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
